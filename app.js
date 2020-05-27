76b1da1f15cbbec093491717a781a058f72fb851
Erick Medina <erickmedinabe@gmail.com>
2020-05-27 16:46:25 +0200

    getting started with core modules

---
+++ app.js
@@ -0,0 +1,11 @@
+/*** Importing core modules
+ * ****/
+
+/*const fs = require('fs');
+
+fs.writeFileSync('hello.txt', 'My name is Erick.');
+
+fs.appendFileSync('hello.txt', ' I am appending some cool stuff');*/
+
+/*** Importing your own files
+ * ****/
