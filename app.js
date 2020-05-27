8dbed5ca5371f9abfa002ccb331704883405d45c
Erick Medina <erickmedinabe@gmail.com>
2020-05-27 18:32:52 +0200

    update on npm

--- app.js
+++ app.js
@@ -9,3 +9,24 @@ fs.appendFileSync('hello.txt', ' I am appending some cool stuff');*/
 
 /*** Importing your own files
  * ****/
+
+/*const firstName = require('./utils');
+
+console.log(firstName);*/
+
+// Challenge
+
+/*const fs = require('fs');
+const notes = require('./notes.js')
+
+// fs.writeFileSync('notes.js', '');
+
+const notesConsole = notes();
+console.log(notesConsole);*/
+
+/*** Importing npm modules
+ * ****/
+
+const validator = require('validator');
+
+console.log(validator.isEmail('erick@hotmail.com'));
---
+++ utils.js
@@ -0,0 +1,3 @@
+const name = 'Erick';
+
+module.exports = name;
